08c86b9dc882d96f9474514c227d6603149be2d9
Himanshu Negi <51152254+Bot-7037@users.noreply.github.com>
2021-04-29 11:11:35 +0530

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,6 +1,6 @@
 pip install numpy
 pip install imutils
-pip insatall cv2
+pip install cv2
 git clone git clone https://github.com/Bot-7037/Bad-Apple.git
 cd Bad-Apple
 python BadApple.py --video videos/videoplayback.m4v
